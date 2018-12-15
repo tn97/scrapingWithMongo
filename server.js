2cdfaa50483d732dbabcf276a20a5a18a265c11d
Timothy Na <runy385@gmail.com>
2018-12-15 15:56:58 -0500

    set up serverjs file

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require("express"),
+  mongoose = require("mongoose"),
+  exphbs = require("express-handlebars"),
+  bodyParser = require("body-parser"),
+  logger = require("morgan"),
+  path = require("path"),
+  favicon = require("serve-favicon");
+
+// initializing the application
+const app = express();
+
+// setting up database
+const config = require("./config/database");
+mongoose.Promise = Promise;
+mongoose
+  .connect(config.database)
+  .then(result => {
+    console.log(`Connected to database '${result.connections[0].name}' on ${result.connections[0].host}:${result.connections[0].port}`);
+  })
+  .catch(err => console.log("There was an error with your connection: ", err));
+
+// setting up the favicon middleware
+app.use(favicon(path.join(__dirname, 'public', 'assets/img/favicon.ico')));
+
+// seting up Morgan middleware
+app.use(logger('dev'));
+
+// setting up body parser middleware
+app.use(bodyParser.json());
+app.use (bodyParser.urlencoded({extended: false}));
+
+// setting up handlebars middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// setting up the static directory
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/articles', express.static(path.join(__dirname, 'public')));
+app.use('/notes', express.static(path.join(__dirname, 'public')));
+
+// setting up routes
+
+const index = require('./routes/index'),
+articles = require('./routes/articles'),
+notes = require('./routes/notes'),
+scrape = require('./routes/scrape');
+
+app.use('/', index);
+app.use('/articles', articles);
+app.use('/notes', notes);
+app.use('/scrape', scrape);
+
+// starting server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on http://localhost:${PORT}`);
+});
