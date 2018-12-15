2b345453409b00654e52eab9656757e53f8125cb
Timothy Na <runy385@gmail.com>
2018-12-15 16:30:15 -0500

    routes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express"),
-  mongoose = require("mongoose"),
-  exphbs = require("express-handlebars"),
-  bodyParser = require("body-parser"),
-  logger = require("morgan"),
-  path = require("path"),
-  favicon = require("serve-favicon");
+      mongoose = require("mongoose"),
+      exphbs = require("express-handlebars"),
+      bodyParser = require("body-parser"),
+      logger = require("morgan"),
+      path = require("path"),
+      favicon = require("serve-favicon");
 
 // initializing the application
 const app = express();
